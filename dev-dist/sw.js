7dc5f3db410b3e2c43e5154bb31f7e4992fae866
ajaykhatidev <ajay@rushclickmedia.com>
2025-10-05 13:08:37 +0530

    logo changed

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.h8ksqlgvadg"
+    "revision": "0.jshsb7eqs9o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
